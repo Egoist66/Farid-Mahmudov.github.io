30b001682eccee44418ec4223106f605658870d3
Farid <razormad666@gmail.com>
2021-11-21 21:41:27 +0300

    20commit

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+// var $section = document.querySelector('.first__section');
+// var checkbox = document.getElementById('checkbox');
+// var label = document.querySelector('label');
+
+// checkbox.addEventListener('click', function () {
+//     var whiteSection = $section.classList.toggle('sectionWhite');
+//     if (whiteSection == true) {
+//         label.style.color = 'white';
+//     } else {
+//         return false
+//     }
+// })
