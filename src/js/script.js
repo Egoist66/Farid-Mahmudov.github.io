7ce38d7dff07019b57f335de0f0f88929300b64c
Farid <razormad666@gmail.com>
2021-11-22 21:02:29 +0300

    6commit

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,8 @@ var listItem = document.querySelector('.navigation__list-anotherSublist');
 var secondItem = document.querySelector('.navigation__list-secondSublist');
 var subList = document.querySelector('.subnavigation__list');
 var underSubList = document.querySelector('.secondsubnavigation__list');
+var details = document.querySelector('details');
+
 
 listItem.addEventListener('click', function () {
     subList.classList.toggle('showBlock');
