5ac31c4bed38977c97f1e23bced7afce4eca00f2
Farid <razormad666@gmail.com>
2021-11-24 18:38:54 +0300

    newcommit

--- src/js/script.js
+++ src/js/script.js
@@ -14,6 +14,50 @@ secondItem.addEventListener('click', function () {
     underSubList.classList.toggle('showBlock');
 })
 
+// menu burger
+
+
+var menuBurger = document.querySelector('#menu__wrapper-hamburgermenu');
+var accordeon = document.querySelector('.accordeon');
+var diableItems = document.querySelectorAll('.diable_list-items');
+
+function rotateSpan() {
+    menuBurger.classList.toggle('open');
+}
+
+
+menuBurger.addEventListener('click', function () {
+    rotateSpan();
+    accordeon.classList.toggle('showed');
+
+})
+
+$(document).ready(function () {
+    $(".navigation__list").find(".accordeon-toggle").click(function () {
+        $(this).next().toggleClass("open").slideToggle("fast");
+        $(this).toggleClass("active-tab").find(".menu-link").toggleClass("active");
+        $('.accordeon-content').css("background", "#232428;");
+        $('.accordeon-content').css("border-radius", "0px");
+
+
+
+
+    })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
