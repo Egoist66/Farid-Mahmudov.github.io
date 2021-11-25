b2317282f575bb96f6cf75748a2188d4ddabb423
Farid <razormad666@gmail.com>
2021-11-26 00:05:19 +0300

    stylechangecommit

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,9 @@ var secondItem = document.querySelector('.navigation__list-secondSublist');
 var subList = document.querySelector('.subnavigation__list');
 var underSubList = document.querySelector('.secondsubnavigation__list');
 var details = document.querySelector('details');
+var menuBurger = document.querySelector('#menu__wrapper-hamburgermenu');
+var accordeon = document.querySelector('.accordeon');
+var diableItems = document.querySelectorAll('.diable_list-items');
 
 
 
@@ -17,6 +20,7 @@ listItem.addEventListener('click', function () {
 
 secondItem.addEventListener('click', function () {
     underSubList.classList.toggle('showBlock');
+
 })
 
 
@@ -31,9 +35,7 @@ secondItem.addEventListener('click', function () {
 // menu burger
 
 
-var menuBurger = document.querySelector('#menu__wrapper-hamburgermenu');
-var accordeon = document.querySelector('.accordeon');
-var diableItems = document.querySelectorAll('.diable_list-items');
+
 
 function rotateSpan() {
     menuBurger.classList.toggle('open');
@@ -50,8 +52,8 @@ $(document).ready(function () {
     $(".navigation__list").find(".accordeon-toggle").click(function () {
         $(this).next().toggleClass("open").slideToggle("fast");
         $(this).toggleClass("active-tab").find(".menu-link").toggleClass("active");
-        $('.accordeon-content').css("background", "#232428;");
         $('.accordeon-content').css("border-radius", "0px");
+
 
     })
 
