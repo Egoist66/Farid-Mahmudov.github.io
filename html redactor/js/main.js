427f7fcb8e741109305186fbdda3775f3158fbce
Farid <razormad666@gmail.com>
2021-12-16 23:25:48 +0300

    save

--- html redactor/js/main.js
+++ html redactor/js/main.js
@@ -4,6 +4,7 @@ var reset = document.querySelector('.reset');
 var content = document.querySelector('#content');
 var createhtml = document.querySelector('.newhtml');
 var li = document.querySelectorAll('li');
+var copy = document.querySelector('.codeCopy');
 
 // main Page Scripts
 
@@ -68,11 +69,16 @@ save.addEventListener('click', function () {
 content.value = window.localStorage.getItem(content.name, content.value);
 
 
-
-
-content.addEventListener('copy', function () {
+function myCopyFunction() {
+    var copyText = document.getElementById("content");
+    copyText.select();
+    document.execCommand("copy");
     alert('Код скопирован');
-})
+}
+
+// content.addEventListener('copy', function () {
+//     alert('Код скопирован');
+// })
 
 
 
@@ -102,6 +108,7 @@ document.querySelector('.downLoad').onclick = function () {
 
 
 
+
 // var block = document.getElementById('block'); block.onclick = function (e) { var point = document.createElement('div'); point.className = 'point'; point.style.left = e.pageX + 'px'; point.style.top = e.pageY + 'px'; block.appendChild(point); } по клики добвлякм
 
 // const allButtonsToStore = document.getElementsByClassName("button");
@@ -113,3 +120,4 @@ document.querySelector('.downLoad').onclick = function () {
 //     console.log(reason[x]);
 //   });
 // }
+
