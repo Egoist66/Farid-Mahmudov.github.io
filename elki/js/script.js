9239212e57d3a8d59c753eda27f7427176cae52e
Farid <razormad666@gmail.com>
2021-12-31 20:22:55 +0300

    elki2

--- elki/js/script.js
+++ elki/js/script.js
@@ -1,4 +1,38 @@
 var img__box = document.querySelectorAll('.img__box');
+var burger = $('.sub__header-burger ');
+var basket = $('.sub__header-basket')
+basket.css('display', 'none');
+
+var hideElements = [$('.sub__header-searchInput'), $('.header__bottom-navigation')]
+
+
+burger.attr('title', 'показать меню');
+hideElements.forEach(hideElement => {
+    hideElement.css('display', 'none');
+})
+
+
+
+burger.click(function () {
+    burger.toggleClass('active');
+    basket.slideToggle(500);
+    for (let i = 0; i < hideElements.length; i++) {
+        hideElements[i].toggle(500);
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 img__box.forEach(items => {
     items.addEventListener('click', function () {
