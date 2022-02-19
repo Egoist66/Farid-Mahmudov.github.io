a51a064299061714aa757c2ee968df04f8364dd0
Farid <razormad666@gmail.com>
2022-02-19 12:21:33 +0300

    newdownside

--- kinoMax/js/script.js
+++ kinoMax/js/script.js
@@ -283,76 +283,44 @@ var aside = document.querySelector('.sidebar');
 
 const mainnavigation = document.getElementById('mainnavigation');
 
-console.log(mainnavigation);
+let downSidebar = document.createElement('aside');
+downSidebar.classList.add('downsidebar');
+downSidebar.innerHTML = `
+                    <div style="background: none" class="search__box sidebarbox">
+                        <form target="_blank" method="get" action="https://fe.it-academy.by/TestForm.php?"
+                            class="form__box">
+                            <h2 style="background: none; text-align: center" class="search_title">Search</h2>
+                            <div style="width: 100%" class="input__box">
+                                <input 
+                                style="width: 70%; border: 1px solid orange; border-right: none; margin: 0 -53px " 
+                                class="downinput" name="search" placeholder="Enter your query" required type="search">
+                                <button style="border: 1px solid orange; border-left: none " type="submit" class="search downsearchbtn"><i class="fas fa-search search_icon"></i></button>
+                            </div>
+                        </form>
+                    </div>
+                    <div style="background: none" class="enter__box sidebarbox">
+                        <h2 style="text-align: center; background: none">LogIn</h2>
+                        <form method="get" action="https://fe.it-academy.by/TestForm.php?" class="enter__form">
+                            <div class="input__enter-box">
+                                <input autocomplete="on" required placeholder="Enter your Email" type="email">
+                                <input required placeholder="Enter your Password" type="password">
+                                <div class="button_box">
+                                    <button value="registered" type="submit" class="login_button">Sign Up</button>
+                                    <button value="logged in" type="submit" class="login_button">Log In</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div style="background: none" class="news__box sidebarbox">
+                        <h2 style="text-align: center; background: none">News</h2>
+                        <div class="news__text">
+                            <p>03.02.2022</p>
+                            <p class="lastP">We launched our new service</p>
+                        </div>
+                    </div>
+                	`;
+Main.appendChild(downSidebar);
 
-// var appearaside = `<aside class="sidebar">
-//                     <div class="search__box sidebarbox">
-//                         <form target="_blank" method="get" action="https://fe.it-academy.by/TestForm.php?"
-//                             class="form__box">
-//                             <h2 class="search_title">Search</h2>
-//                             <div class="input__box">
-//                                 <input name="search" placeholder="Enter your query" required type="search">
-//                                 <button type="submit" class="search"><i class="fas fa-search search_icon"></i></button>
-//                             </div>
-//                         </form>
-//                     </div>
-//                     <div class="enter__box sidebarbox">
-//                         <h2>LogIn</h2>
-//                         <form method="get" action="https://fe.it-academy.by/TestForm.php?" class="enter__form">
-//                             <div class="input__enter-box">
-//                                 <input autocomplete="on" required placeholder="Enter your Email" type="email">
-//                                 <input required placeholder="Enter your Password" type="password">
-//                                 <div class="button_box">
-//                                     <button value="registered" type="submit" class="login_button">Sign Up</button>
-//                                     <button value="logged in" type="submit" class="login_button">Log In</button>
-//                                 </div>
-//                             </div>
-//                         </form>
-//                     </div>
-//                     <div class="news__box sidebarbox">
-//                         <h2>News</h2>
-//                         <div class="news__text">
-//                             <p>03.02.2022</p>
-//                             <p class="lastP">We launched our new service</p>
-//                         </div>
-//                     </div>
-//                     <div class="news__box ratings__box sidebarbox">
-//                         <h2>Ratings</h2>
-//                         <ul class="ratingsFilm__list">
-//                             <div class="ratings__box ">
-//                                 <li><a href="movies/interstellar.html" class="ratingsFilm__list-links">Interstellar</a>
-//                                 </li>
-//                                 <p class="firstrating">9.0</p>
-//                                 <li><a href="movies/avengers.html" class="ratingsFilm__list-links">Avengers</a></li>
-//                                 <p class="secondrating">9.8</p>
-//                                 <li><a href="movies/madmax.html" class="ratingsFilm__list-links">Mad Max</a></li>
-//                                 <p class="thirdtrating">9.1</p>
-//                                 <li class="lastLi"><a href="movies/freeguy.html" class="ratingsFilm__list-links">Free
-//                                         Guy</a></li>
-//                                 <p class="fourthrating">8.9</p>
-//                             </div>
-//                         </ul>
-//                     </div>
-//                     <div class="news__box ratings__box serials__box sidebarbox">
-//                         <h2>Serials</h2>
-//                         <ul class="ratingsFilm__list serials__list">
-//                             <div class="ratings__box serials__box ">
-//                                 <li><a href="serials/breakingBad.html"
-//                                         class="ratingsFilm__list-links serials__list-links">Breaking Bad</a>
-//                                 </li>
-//                                 <p class="firstrating">9.5</p>
-//                                 <li><a href="serials/xfiles.html" class="ratingsFilm__list-links">The X Files</a></li>
-//                                 <p class="secondrating">8.7</p>
-//                                 <li><a href="serials/walkingDead.html" class="ratingsFilm__list-links">Walking Dead</a>
-//                                 </li>
-//                                 <p class="thirdtrating">8.6</p>
-//                                 <li class="lastLi"><a href="serials/siliconvalley.html"
-//                                         class="ratingsFilm__list-links">Silicon Valley</a></li>
-//                                 <p class="fourthrating">8.5</p>
-//                             </div>
-//                         </ul>
-//                     </div>
-//                 </aside>`;
 
 
 
